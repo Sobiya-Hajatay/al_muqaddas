1f172f6cd992b8581ed4a9e01b8538a7b828078d
Sobiya-Hajatay <hajataysobiya@gmail.com>
2026-03-09 01:08:50 +0530

    Fix homepage

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from . import views
+from .views import home
 
 
 urlpatterns = [
 
     path("contact/", views.contact, name="contact"),
+    path('', views.home, name='home'),
+    path("", home, name="home"),
     
 
 
