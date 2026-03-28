1a29a8819c1335474ce1dc3ea27136a6d380a993
Sobiya-Hajatay <hajataysobiya@gmail.com>
2026-03-28 23:03:08 +0530

    fix middleware and static for render

--- config/settings/base.py
+++ config/settings/base.py
@@ -31,15 +31,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 👈 yaha hona chahiye
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
