2babfadcad5ef3fd61eb7f40ed6c146ecd514c49
Sobiya-Hajatay <hajataysobiya@gmail.com>
2026-03-29 00:57:07 +0530

    simplified user admin form

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    
+    list_display = ('username', 'phone', 'role')
+
+    # 👇 form fields clean kar diya
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('phone', 'role', 'wallet_balance')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'phone', 'role'),
+        }),
+    )
+
+    # 👇 ye sab hata diya
+    exclude = ('groups', 'user_permissions', 'last_login', 'is_superuser', 'is_staff')
